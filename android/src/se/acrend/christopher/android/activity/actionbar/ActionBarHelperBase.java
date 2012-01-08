55f404da43451f36d602acc30796d5d4b0259e66
Johan Kindgren <johan.kindgren@acrend.se>
2012-01-08 22:54:02 +0100

    home-logo

--- android/src/se/acrend/christopher/android/activity/actionbar/ActionBarHelperBase.java
+++ android/src/se/acrend/christopher/android/activity/actionbar/ActionBarHelperBase.java
@@ -82,7 +82,7 @@ public class ActionBarHelperBase extends ActionBarHelper {
     SimpleMenu tempMenu = new SimpleMenu(mActivity);
     SimpleMenuItem homeItem = new SimpleMenuItem(
         tempMenu, android.R.id.home, 0, mActivity.getString(R.string.app_name));
-    homeItem.setIcon(R.drawable.ic_launcher_logo);
+    homeItem.setIcon(R.drawable.ic_logo);
     addActionItemCompatFromMenuItem(homeItem);
 
     // Add title text
